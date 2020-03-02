3b18b0da9b80c54b485b0e1df12d1abc5975b211
AbslanLaverde <xavier0286@gmail.com>
2020-03-02 00:00:36 -0500

    Updated environment data
    restarted ec2 - url changed - environment.url has been changed to reflect new ec2 address

--- Crescendo-Client-side/src/environments/environment.prod.ts
+++ Crescendo-Client-side/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'http://ec2-18-222-201-71.us-east-2.compute.amazonaws.com:8081/Crescendo'
+  url: 'http://ec2-18-216-1-145.us-east-2.compute.amazonaws.com:8081/Crescendo'
 };
