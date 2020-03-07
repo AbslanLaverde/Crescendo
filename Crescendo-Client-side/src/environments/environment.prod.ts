8f13d7e8b3bde95c83371efd248856f7150f4c69
AbslanLaverde <xavier0286@gmail.com>
2020-03-06 23:54:53 -0500

    updated env url

--- Crescendo-Client-side/src/environments/environment.prod.ts
+++ Crescendo-Client-side/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  url: 'http://ec2-18-216-1-145.us-east-2.compute.amazonaws.com:8081/Crescendo'
+  url: 'http://ec2-18-216-1-145.us-east-2.compute.amazonaws.com:8081/Crescendo' 
+  
 };
